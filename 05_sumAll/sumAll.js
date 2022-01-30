0d8f9073596193bf6a331df9087b03e696980e09
gilles <gilles187@hotmail.com>
2022-01-30 21:24:05 +0100

    js exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    if (a < 0 || b < 0) {
+        return "ERROR"
+    }
+    if (typeof (a) != "number" || typeof (b) != "number") {
+        return "ERROR"
+    }
+    if (a > b) {
+        const tmp = a
+        a = b
+        b = tmp
+    }
+    return (a + b) * (b - a + 1) / 2
 };
 
 // Do not edit below this line
