0e09ddfb5e00ae632b7c753976d1faa57d0cedd6
gilles <gilles187@hotmail.com>
2022-01-30 17:44:17 +0100

    js exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const chars = string.split("");
+    let reversed = ""
+    for (const char of chars) {
+        reversed = char + reversed;
+    }
+    return reversed;
 };
 
 // Do not edit below this line
