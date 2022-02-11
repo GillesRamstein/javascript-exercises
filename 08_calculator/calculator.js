c03d01671bc82cf5a9cb196a3db1d1753c28bd50
Gilles Ramstein <gilles187@hotmail.com>
2022-02-11 22:15:01 +0100

    ex8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b 
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((j, i) => i+j, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((j, i) => i * j, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = a > 0 ? a : 1
+  while (a > 1) {
+    result *= --a
+  }
+  return result
 };
 
 // Do not edit below this line
