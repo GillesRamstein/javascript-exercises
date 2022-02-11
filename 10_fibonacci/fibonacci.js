3a02cf2b21f6b2428982e205653fe2a336019898
Gilles Ramstein <gilles187@hotmail.com>
2022-02-11 23:04:55 +0100

    ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
+// const fibonacci = function(a) {
+//     if (a < 0) return "OOPS"
+//     let prev = 1
+//     let current = 1
+//     let result = 1
+//     for (let i=2; i<a; i++) {
+//         result = prev + current
+//         prev = current
+//         current = result
+//     }
+//     return result
+// };
 
+const fibonacci = function(a) {
+    // f(0) = 1
+    // f(1) = 1
+    // f(n) = f(n-2) + f(n-1)
+    if (a < 0) return "OOPS"
+    a = Number(a)
+    if (a <= 1) return a
+    return fibonacci(a-2) + fibonacci(a-1)
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
