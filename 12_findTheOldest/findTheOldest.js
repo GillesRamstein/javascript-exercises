4f4847bc34c2f0c6c1b936226bae9b955a4fb076
Gilles Ramstein <gilles187@hotmail.com>
2022-02-11 23:26:32 +0100

    ex12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.sort((a, b) => age(a) > age(b) ? -1 : 1)[0]
 };
+
+function age(person) {
+    if ("yearOfDeath" in person) {
+        return person.yearOfDeath - person.yearOfBirth
+    } else {
+        return (new Date()).getFullYear() - person.yearOfBirth
+    }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
