9658647b2e25a84dbc1d6ac0e8ef25f6d53173ff
Gilles Ramstein <gilles187@hotmail.com>
2022-02-11 22:33:22 +0100

    ex9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleaned = string.replace(/\W/g, "").toLowerCase()
+    const reversed = cleaned.split("").reverse().join("")
+    return cleaned === reversed
 };
 
 // Do not edit below this line
